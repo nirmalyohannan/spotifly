b512b77d47b5c86c5be6fb02f54b814c52d19582
Nirmal Yohannan <nirmalyohannan.in@gmail.com>
2025-12-04 20:19:03 +0530

    feat: Introduce `Debouncer` utility and apply it to the search input to delay queries.

--- lib/features/search/presentation/pages/search_page.dart
+++ lib/features/search/presentation/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotifly/core/utils/debouncer.dart';
 import 'package:spotifly/features/search/presentation/widgets/search_initial_view.dart';
 import 'package:spotifly/features/search/presentation/widgets/search_loaded_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -28,10 +29,12 @@ class _SearchViewState extends State<SearchView> {
   final TextEditingController _searchController = TextEditingController();
   final FocusNode _focusNode = FocusNode();
   bool _isSearching = false;
+  late final Debouncer _debouncer;
 
   @override
   void initState() {
     super.initState();
+    _debouncer = Debouncer(milliseconds: 500);
     _focusNode.addListener(() {
       setState(() {
         _isSearching = _focusNode.hasFocus || _searchController.text.isNotEmpty;
@@ -43,6 +46,7 @@ class _SearchViewState extends State<SearchView> {
   void dispose() {
     _searchController.dispose();
     _focusNode.dispose();
+    _debouncer.dispose();
     super.dispose();
   }
 
@@ -89,9 +93,13 @@ class _SearchViewState extends State<SearchView> {
                         : null,
                   ),
                   onChanged: (value) {
-                    if (value.length >= 2) {
-                      context.read<SearchBloc>().add(SearchQueryChanged(value));
-                    }
+                    _debouncer.run(() {
+                      if (value.length >= 2) {
+                        context.read<SearchBloc>().add(
+                          SearchQueryChanged(value),
+                        );
+                      }
+                    });
                   },
                 ),
               ),
