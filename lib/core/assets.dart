525fbb9018ef21c000e2d8baf655ad14c635072b
Nirmal Yohannan <nirmalyohannan.in@gmail.com>
2025-12-13 16:22:06 +0530

    feat: Introduce Lottie package and 'isPlaying' animation for current song indicator in playlist detail.

---
+++ lib/core/assets.dart
@@ -0,0 +1,8 @@
+class Assets {
+  static const lotties = Lotties._();
+}
+
+class Lotties {
+  const Lotties._();
+  final String isPlaying = 'assets/lotties/isPlaying.json';
+}
--- lib/features/library/presentation/pages/playlist_detail_page.dart
+++ lib/features/library/presentation/pages/playlist_detail_page.dart
@@ -1,8 +1,13 @@
+import 'dart:developer';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:spotifly/core/assets.dart';
 import 'package:spotifly/core/di/service_locator.dart';
 import 'package:spotifly/core/theme/app_colors.dart';
 import 'package:spotifly/core/utils/flight_shuttle_builder.dart';
+import 'package:spotifly/features/player/presentation/bloc/player_state.dart';
 import 'package:spotifly/shared/domain/repositories/playlist_repository.dart';
 import '../../../../shared/domain/entities/playlist.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -101,33 +106,51 @@ class _PlaylistDetailPageState extends State<PlaylistDetailPage> {
                     SliverList(
                       delegate: SliverChildBuilderDelegate((context, index) {
                         final song = playlist.songs[index];
-                        return ListTile(
-                          leading: CachedNetworkImage(
-                            imageUrl: song.coverUrl,
-                            width: 50,
-                            height: 50,
-                            fit: BoxFit.cover,
-                            errorWidget: (context, error, stackTrace) =>
-                                Container(
+                        return BlocSelector<PlayerBloc, PlayerState, bool>(
+                          selector: (state) {
+                            return state.currentSong?.id == song.id;
+                          },
+                          builder: (context, isPlaying) {
+                            return AnimatedSize(
+                              duration: const Duration(milliseconds: 300),
+                              child: ListTile(
+                                leading: CachedNetworkImage(
+                                  imageUrl: song.coverUrl,
                                   width: 50,
                                   height: 50,
-                                  color: Colors.grey,
-                                  child: const Icon(Icons.music_note),
+                                  fit: BoxFit.cover,
+                                  errorWidget: (context, error, stackTrace) =>
+                                      Container(
+                                        width: 50,
+                                        height: 50,
+                                        color: Colors.grey,
+                                        child: const Icon(Icons.music_note),
+                                      ),
                                 ),
-                          ),
-                          title: Text(
-                            song.title,
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                          subtitle: Text(
-                            song.artist,
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                          onTap: () {
-                            context.read<PlayerBloc>().add(
-                              SetPlaylistEvent(
-                                songs: playlist.songs,
-                                initialIndex: index,
+                                title: Text(
+                                  song.title,
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                                subtitle: Text(
+                                  song.artist,
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
+                                trailing: isPlaying
+                                    ? LottieBuilder.asset(
+                                        Assets.lotties.isPlaying,
+                                        repeat: true,
+                                        width: 50,
+                                        height: 50,
+                                      )
+                                    : null,
+                                onTap: () {
+                                  context.read<PlayerBloc>().add(
+                                    SetPlaylistEvent(
+                                      songs: playlist.songs,
+                                      initialIndex: index,
+                                    ),
+                                  );
+                                },
                               ),
                             );
                           },
